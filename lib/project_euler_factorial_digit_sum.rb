689499447fdf9e0f4ddf1ed4e3e455e1d9fdb586
cloudleo <l.castillo1989@hotmail.com>
2015-11-29 19:49:49 -0500

    love this lab

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,27 @@
 # code your solution here
+require "pry"
+def factorial(num)
+ var = []
+ num.times do |i|
+   var << (i + 1)
+
+  end
+  bob = var.inject(:*)
+return bob
+end
+
+
+
+def sum_of_digits(arg)
+  p = 0
+arg.to_s.split("").each {|m| p +=  m.to_i }
+ return p
+end
+
+
+def factorial_digit_sum(arg)
+bob = factorial(arg)
+var = sum_of_digits(bob)
+ return var
+
+end
