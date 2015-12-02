066fc9760a840ee5760399855a5ee424af8a2450
github username <mendelblesofsky@gmail.com>
2015-12-01 19:07:58 -0500

    digital sum

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,12 @@
 # code your solution here
+def factorial(num)
+	(1...num).to_a.reverse.inject(num) { |sum, n| sum = (sum*n) }
+end
+
+def sum_of_digits(num)
+	num.to_s.split('').map(&:to_i).inject { |sum, n| sum += n }
+end
+
+def factorial_digit_sum(num)
+	sum_of_digits(factorial(num))
+end
